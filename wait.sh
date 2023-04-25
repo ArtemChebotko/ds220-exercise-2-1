137f33e69fd1fc6cf786cfe3ef47cc0d0e47afc4
Artem Chebotko <artemchebotko@gmail.com>
2023-04-25 17:29:21 -0500

    Update wait.sh

--- wait.sh
+++ wait.sh
@@ -10,7 +10,7 @@ echo " ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚�
 echo ""
 
 echo -n 'Starting a Cassandra cluster ...'; 
-timeout 60 bash -c 'until cqlsh -e "describe cluster" >/dev/null 2>&1; do sleep 2; echo -n "."; done'; 
+timeout 120 bash -c 'until cqlsh -e "describe cluster" >/dev/null 2>&1; do sleep 2; echo -n "."; done'; 
 echo ' DONE!'
 echo "Cassandra successfully started."
 
